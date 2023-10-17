49cdf89d70d1c740438155fd7c84f6c3416ce55a
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-17 19:04:20 +0200

    design helpfooter

--- source/components/HelpFooter.tsx
+++ source/components/HelpFooter.tsx
@@ -2,22 +2,33 @@ import React from 'react'
 import {CME} from './keybindings/keybindings.js'
 import {Box, Text} from 'ink'
 
-export function HelpFooter({map}: {map: {[key: string]: CME}}) {
+type HelpFooterMap = {[key: string]: CME}
+type MapEntry = [string, CME]
+type MapEntries = Array<MapEntry>
+
+export function HelpFooter({map}: {map: HelpFooterMap}) {
+	const entries: MapEntries = Object.entries(map)
 	return (
-		<>
-			<Box>
-				{Object.entries(map).map((entry, index) => (
-					<HelpText data={entry} key={index} />
-				))}
-			</Box>
-		</>
+		<Box
+			width="100%"
+			borderStyle={'single'}
+			borderBottom={false}
+			borderLeft={false}
+			borderRight={false}
+			flexDirection="row"
+			justifyContent="space-around"
+		>
+			{entries.map((entry, index) => (
+				<HelpText data={entry} key={index} />
+			))}
+		</Box>
 	)
 }
 
-function HelpText({data}: {data: [string, CME]}) {
+function HelpText({data}: {data: MapEntry}) {
 	return (
-		<Box>
-			<Text>{data[0]}</Text>
+		<Box justifyContent="center" flexGrow={1} gap={1}>
+			<Text bold>{data[0]}</Text>
 			<Text>{data[1].d}</Text>
 		</Box>
 	)
