b0e30b9821cd2e63f8cf323408eaf328f84715a5
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-18 15:09:39 +0200

    wip runImage

--- source/components/routes/runImage.tsx
+++ source/components/routes/runImage.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {ImgSubProps} from './Image.js'
 import {Box, Text} from 'ink'
 
 export function RunImg({sel}: ImgSubProps) {
+	const [host, setHost] = useState<string | null>(null)
+	const [local, setLocal] = useState<string | null>(null)
+	const [t, setT] = useState<boolean>(false)
+	const [i, setI] = useState<boolean>(false)
 	return (
 		<Box>
-			<Text>{sel}</Text>
+			{!host ? (
+				<Port setter={setHost} value={host} />
+			) : !local ? (
+				<Port setter={setLocal} value={local} />
+			) : (
+				<Options setT={setT} setI={setI} />
+			)}
 		</Box>
 	)
+}
+
+type PortProps = {setter: Function; value: string | null}
+type Options = {setT: Function; setI: Function}
+
+function Port({setter, value}: PortProps) {
+	return <Box></Box>
+}
+
+function Options({setT, setI}: OptionsProps) {
+	return <Box></Box>
 }
