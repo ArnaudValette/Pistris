ec21a9730f576a2e4e1e2fa719e498e6a02c744b
ArnaudValette <valette.arnaud.ak@gmail.com>
2023-10-20 16:30:17 +0200

    adds kill, remove, start container Components

---
+++ source/components/routes/Containers/RmContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {SubProps} from '../../Routes.js'
+import {Box} from 'ink'
+
+export function RmContainer(props: SubProps) {
+	console.log(props)
+	return <Box></Box>
+}
